e570d4221d25b12c2669c4f87aba67b49fe2cb55
krisamorim <kris.a.furtado@gmail.com>
2024-04-19 11:32:44 -0300

    forEach and multidimentional arrays

---
+++ MOD01/src/leson04TypesArrayMutidimanetional.java
@@ -0,0 +1,17 @@
+public class leson04TypesArrayMutidimanetional {
+    public static void main(String[] args) {
+        int[][] numbers = {{1,2},{3,4,5},{6,7,8,9}};
+//        System.out.println(numbers[0]); //position on memory
+//        System.out.println(numbers[0][0]);
+//        System.out.println(numbers[1][0]);
+
+        var count = 1;
+        for (int[] num : numbers){// it's necessary inform []
+            System.out.println("Group "+count+":");
+            count++;
+            for(int n : num){
+                System.out.println(n);
+            }
+        }
+    }
+}
---
+++ MOD01/src/leson08forEach.java
@@ -0,0 +1,8 @@
+public class leson08forEach {
+    public static void main(String[] args){
+        float[] numbers = {10, 20, 30};
+        for (float num : numbers){
+            System.out.println(num);
+        }
+    }
+}
