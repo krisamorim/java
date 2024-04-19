10f320d7178ee370843339f9bf66468393674d35
krisamorim <kris.a.furtado@gmail.com>
2024-04-19 09:39:49 -0300

    Types - Arrays

---
+++ MOD01/src/leson04TypesArray.java
@@ -0,0 +1,15 @@
+public class leson04TypesArray {
+    public static void main(String[] args) {
+        int[] age = new int[3]; //int
+        age[0] = 10;
+        age[1] = 20;
+        age[2] = 30;
+        System.out.println(age[0]);
+
+        float[] numberArray2 = new float[3];//float
+        numberArray2[0] = 10;
+        numberArray2[1] = 20;
+        numberArray2[2] = 30;
+        System.out.println(numberArray2[0]);
+    }
+}
