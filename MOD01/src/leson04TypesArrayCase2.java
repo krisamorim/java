65b5a1852022cb71b090714649a4127b72e6fc5e
krisamorim <kris.a.furtado@gmail.com>
2024-04-19 09:53:18 -0300

    remove file

--- MOD01/src/leson04TypesArrayCase2.java
+++
@@ -1,8 +0,0 @@
-public class leson04TypesArrayCase2 {
-    public static void main(String[] args) {
-        int[] numbers = {0,1,2,3,4,5};
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(i);
-        }
-    }
-}
