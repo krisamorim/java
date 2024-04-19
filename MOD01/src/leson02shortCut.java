0190af47900d28b641d45e96f55abf1e7e202bca
krisamorim <kris.a.furtado@gmail.com>
2024-04-19 09:52:19 -0300

    Types - Arrays and shortcuts

--- MOD01/src/leson02shortCut.java
+++ MOD01/src/leson02shortCut.java
@@ -2,5 +2,9 @@ public class leson02shortCut {
     public static void main(String[] args) {
         System.out.println("psvm + tab");
         System.out.println("sout + tab");
+        System.out.println("fori + tab");
+        System.out.println("CTRL+SHIFT+F10 to RUN");
+        System.out.println("CTRL+K to COMMIT");
+        System.out.println("CTRL+SHIFT+K to PUSH");
     }
 }
--- MOD01/src/leson04TypesArray.java
+++ MOD01/src/leson04TypesArray.java
@@ -1,15 +1,19 @@
 public class leson04TypesArray {
     public static void main(String[] args) {
+        //CASE1
         int[] age = new int[3]; //int
         age[0] = 10;
         age[1] = 20;
         age[2] = 30;
         System.out.println(age[0]);
 
-        float[] numberArray2 = new float[3];//float
-        numberArray2[0] = 10;
-        numberArray2[1] = 20;
-        numberArray2[2] = 30;
+        //CASE2
+        float[] numberArray2 = new float[]{10,20,30};//float
         System.out.println(numberArray2[0]);
+
+        //CASE3
+        float[] numberArray3 = {10,20,30};//float
+        System.out.println(numberArray2[0]);
+
     }
 }
