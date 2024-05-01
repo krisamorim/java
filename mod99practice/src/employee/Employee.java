9dfb3b3c84140a78e5c739a4c64c7d3df171f9b3
krisamorim <kris.a.furtado@gmail.com>
2024-05-01 10:09:26 -0300

    mods

--- mod99practice/src/employee/Employee.java
+++ mod99practice/src/employee/Employee.java
@@ -1,2 +1,20 @@
-package employee;public class Employee {
+package employee;
+
+public class Employee {
+    public String name;
+    public int age;
+    public double[] payment;
+
+    public void printIt(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+        for (double n : payment){
+            System.out.print(n+",");
+        }
+    }
+    public void averga(){
+        for( n : payment){
+
+        }
+    }
 }
--- mod99practice/src/employee/Test.java
+++ mod99practice/src/employee/Test.java
@@ -1,2 +1,12 @@
-package employee;public class Test {
+package employee;
+
+public class Test {
+    public static void main(String[] args) {
+        Employee employee01 = new Employee();
+        employee01.name = "Kris";
+        employee01.age = 40;
+        employee01.payment = new double[] {8000, 10000, 12000};
+
+        employee01.printIt();
+    }
 }
